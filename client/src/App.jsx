7f63626bb8c7942c571b912353a5456df81ca96e
Mariana <am.du.mariana@gmail.com>
2025-04-15 13:34:54 +0100

    Remove commented-out code and clean up imports in App and MyPlayerForm components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,77 +1,3 @@
-// import { EmpiricaClassic } from "@empirica/core/player/classic";
-// import { EmpiricaContext } from "@empirica/core/player/classic/react";
-// import { EmpiricaMenu, EmpiricaParticipant } from "@empirica/core/player/react";
-// import React from "react";
-// import { Game } from "./Game";
-// import { ExitSurvey } from "./intro-exit/ExitSurvey";
-// import { Introduction } from "./intro-exit/Introduction";
-// import { MyConsent } from "./intro-exit/Consent";
-// import { PreTask } from "./intro-exit/PreTask";
-// import { PostQuestions } from "./intro-exit/PostQuestions";
-// import { PostSurvey } from "./intro-exit/PostSurvey";
-// import { TypingSpeedTest } from "./intro-exit/TypingSpeedTest";
-// import { IntroductionInterleaved } from "./intro-exit/IntroductionInterleaved";
-// import { IntroductionSelfinitiated } from "./intro-exit/IntroductionSelfinitiated";
-// import { FinalScoreSummary } from "./intro-exit/FinalScoreSummary";
-
-// export default function App() {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const playerKey = urlParams.get("participantKey") || "";
-
-//   const { protocol, host } = window.location;
-//   const url = `${protocol}//${host}/query`;
-
-//   // function introSteps({ game, player }) {
-//   //   // return [MyConsent, TypingSpeedTest, PreTask, Introduction,];
-//   //   return [Introduction];
-//   // }
-
-//   function introSteps({ game, player }) {
-//     const treatment = game.get("treatment");
-//     const taskType = treatment.taskType;
-//     return [
-//       MyConsent, PreTask, TypingSpeedTest,
-//       taskType === "interleaved" ? IntroductionInterleaved : IntroductionSelfinitiated
-//     ];
-//   }
-
-
-//   function exitSteps({ game, player }) {
-//     return [PostSurvey, PostQuestions, FinalScoreSummary];
-//   }
-
-//   function consent() {
-//     return {
-//       component: MyConsent,
-//       onConsent: () => {
-//         console.log("Consent given");
-//         // You can add any other logic here that you want to happen when consent is given
-//       }
-//     };
-//   }
-  
-//   //add redirect to prolific if consent is not given
-
-
-//   return (
-//     <EmpiricaParticipant url={url} ns={playerKey} modeFunc={EmpiricaClassic}>
-//       <div className="h-screen relative">
-//         <EmpiricaMenu position="bottom-left" />
-//         <div className="h-full overflow-auto">
-//           <EmpiricaContext 
-//             consent={consent} 
-//             introSteps={introSteps} 
-//             exitSteps={exitSteps}
-//           >
-//             <Game />
-//           </EmpiricaContext>
-//         </div>
-//       </div>
-//     </EmpiricaParticipant>
-//   );
-// }
-
-
 import { EmpiricaClassic } from "@empirica/core/player/classic";
 import { EmpiricaContext } from "@empirica/core/player/classic/react";
 import { EmpiricaMenu, EmpiricaParticipant } from "@empirica/core/player/react";
@@ -121,6 +47,7 @@ export default function App() {
   //   return [
   //     taskType === "interleaved" ? IntroductionInterleaved : IntroductionSelfinitiated
   //   ];
+
   }
 
   // function exitSteps({ game, player }) {
@@ -155,17 +82,6 @@ export default function App() {
       return [FailedGame];
     }
   }
-
-  //actually there is also "lobby timed out" and "no more games"
-
-
-  //   // Add this consent configuration function
-  // const consentConfig = {
-  //   consent: MyConsent,
-  //   onConsent: () => {
-  //     console.log("Consent processed in App");
-  //   }
-  // };
 
   return (
     <EmpiricaParticipant url={url} ns={playerKey} modeFunc={EmpiricaClassic}>
